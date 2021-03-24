ce3868d9bb146ac5886abd434d928e07826f5bc6
Arief Setiawan <ariefbc@gmail.com>
2021-03-24 19:00:11 +0700

    omitted cache and modified background

--- application/views/Vwlogin.php
+++ application/views/Vwlogin.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>ePMAP</title>
+    <title>FOS</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/font-awesome.min.css">
@@ -14,10 +14,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/iCheck/square/blue.css">
 
   </head>
-  <body class="hold-transition login-page" style="background-image: url('<?php echo base_url();?>assets/images/bg_login.png');background-repeat: no-repeat;background-size: 100% 100%;">
+  <body class="hold-transition login-page" style="background-image: url('<?php echo base_url();?>assets/images/bg_login.jpg');background-repeat: no-repeat;background-size: cover; background-attachment: fixed">
     <div class="login-box">
       <div class="login-logo">
-        <b><font color="orange">e-PMAP 1.0</font></b>
+        <strong><font color="white">Food Ordering System 1.0</font></strong>
       </div>
       <div class="login-box-body">
         <p class="login-box-msg"><?php echo $title; ?></p>
@@ -44,17 +44,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <br>
     <br>
     <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <script src="<?php echo base_url();?>assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
     <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>assets/plugins/iCheck/icheck.min.js"></script>
     <script>
-      $(function () {
-        $('input').iCheck({
-          checkboxClass: 'icheckbox_square-blue',
-          radioClass: 'iradio_square-blue',
-          increaseArea: '20%'
-        });
-      });
       <?php
         if (isset($session_expired_msg)) {
           if ($session_expired_msg != "") {
